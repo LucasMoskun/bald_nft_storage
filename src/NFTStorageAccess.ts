3cb853bc038a9c5d3dd3d8a91525fb5c7fd1dd32
LucasMoskun <contact@lucasmoskun.com>
2021-08-09 22:44:24 +0100

    attempt to load png

--- src/NFTStorageAccess.ts
+++ src/NFTStorageAccess.ts
@@ -1,20 +1,51 @@
 import { STORAGE_API_KEY } from "./STORAGE_API_KEY.js";
 import { NFTStorage, File } from "nft.storage";
+//jimport PNG from 'png-ts';
+import * as pug from './../src/pug.png'
+//const pug = require("pug.png");
 
 export class NFTStorageAccess {
     constructor()
     {
         this.logHello();
-        this.logAPIKey();
+        //this.logAPIKey();
+        //this.uploadNFT();
     }
 
     logHello()
     {
         console.log("Hello from storage class")
+        console.log(pug)
     }
 
     logAPIKey()
     {
         console.log(STORAGE_API_KEY);
+    }
+
+    async uploadNFT()
+    {
+        const client = new NFTStorage({ token: STORAGE_API_KEY })
+        
+
+        const fileUri =  "/Users/lucas/bald/bald_nft_storage/dist/pug.png";
+
+        const httpResponse = await fetch(fileUri);
+        const buffer: ArrayBuffer = await httpResponse.arrayBuffer();
+        const bytes: Uint8Array = new Uint8Array(buffer);
+
+        const metadata = await client.store({
+            name: 'pug_life',
+            description: 'A fearless pug',
+            image: new File([bytes], 'pug.png', { type: 'image/png' }),
+            properties: {
+                cuteness: 'Off the charts',
+                friendlieness: 'limitless'
+            }
+        })
+
+        console.log('IPFS URL for the metadata:', metadata.url)
+        console.log('metadata.json contents:\n', metadata.data)
+        console.log('metadata.json with IPFS gateway URLs:\n', metadata.embed())
     }
 }
