77af0bb618bd877e490aac387d2f98df520ff1d8
LucasMoskun <contact@lucasmoskun.com>
2021-08-14 22:45:55 +0100

    added rinkeby paramaters to hardhat config

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,8 +1,23 @@
-require("@nomiclabs/hardhat-waffle")
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+require('dotenv').config();
+require("@nomiclabs/hardhat-waffle")
+const {API_URL, METAMASK_PRIVATE_KEY} = process.env;
 module.exports = {
   solidity: "0.8.0",
+  defaultNetwork: "rinkeby",
+  networks: {
+    hardhat: {},
+    rinkeby: {
+      url: API_URL,
+      accounts: [`0x${METAMASK_PRIVATE_KEY}`]
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
 };
