c11b8fcad037a170bf1103843474d2f299002048
LucasMoskun <contact@lucasmoskun.com>
2021-08-14 16:47:37 +0100

    upgraded to solidity 0.8.0, override erc721 storage

--- smart_contract/contracts/CreepKidsNFT.sol
+++ smart_contract/contracts/CreepKidsNFT.sol
@@ -1,14 +1,32 @@
-pragma solidity ^0.7.3;
+pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol"
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract CreepKidsNFT is ERC721, Ownable {
+contract CreepKidsNFT is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
-    Counter.Counter privated TokenIds;
+    Counters.Counter private TokenIds;
 
     constructor() public ERC721("CreepKidsNFT", "CK") {}
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256  tokenId)
+    public
+    view
+    override(ERC721, ERC721URIStorage)
+    returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
 
     function CreateNFT(address receiver, string memory tokenURI)
         public onlyOwner
@@ -16,8 +34,8 @@ contract CreepKidsNFT is ERC721, Ownable {
     {
         TokenIds.increment();
 
-        unit256 newID = _tokenIds.current();
-        _mint(receiver, newItemId);
+        uint256 newID = TokenIds.current();
+        _mint(receiver, newID);
         _setTokenURI(newID, tokenURI);
 
         return newID;
