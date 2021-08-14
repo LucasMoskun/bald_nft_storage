0d57154e5b9baa0a318833356ebc48fe9c42f510
LucasMoskun <contact@lucasmoskun.com>
2021-08-14 23:35:52 +0100

    ability to deploy smart contract to rinkeby

---
+++ smart_contract/scripts/deploy.js
@@ -0,0 +1,17 @@
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts woth the account: ", deployer.address);
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+  const contract = await ethers.getContractFactory("CreepKidsNFT");
+
+  const result = await contract.deploy();
+  console.log("Contract deployed to address: ", result.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
