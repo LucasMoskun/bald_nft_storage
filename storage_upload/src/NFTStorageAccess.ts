f6f6d17e5c72b9e772c23394ff1d204fa5def4a0
LucasMoskun <contact@lucasmoskun.com>
2021-08-15 14:19:28 +0100

    removed debug/comments from nft storage access

--- storage_upload/src/NFTStorageAccess.ts
+++ storage_upload/src/NFTStorageAccess.ts
@@ -1,33 +1,17 @@
 import { STORAGE_API_KEY } from "./STORAGE_API_KEY.js";
 import { NFTStorage, File } from "nft.storage";
 import fetch from "node-fetch";
-//jimport PNG from 'png-ts';
-//import * as pug from './../src/pug.png'
-//const pug = require("pug.png");
 
 export class NFTStorageAccess {
     constructor()
     {
-        this.logHello();
-        //this.logAPIKey();
         this.uploadNFT();
-    }
-
-    logHello()
-    {
-        console.log("Hello from storage class")
-    }
-
-    logAPIKey()
-    {
-        console.log(STORAGE_API_KEY);
     }
 
     async uploadNFT()
     {
         const client = new NFTStorage({ token: STORAGE_API_KEY })
         
-
         const fileUri =  "http://127.0.0.1:8080/demon.gif";
 
         const httpResponse = await fetch(fileUri);
