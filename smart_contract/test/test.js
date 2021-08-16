838aa393dd50c8144f128bd5dd0e0eab37d95066
LucasMoskun <contact@lucasmoskun.com>
2021-08-16 18:41:13 +0100

    initial implementation of smart contract test suit

--- smart_contract/test/test.js
+++ smart_contract/test/test.js
@@ -5,16 +5,20 @@ describe("Creep Kid Test", function() {
   it("Running tests on creep kids nft", async function () {
 
     console.log("Getting signers");
-    const [owner] = await ethers.getSigners();
+    //const [owner, addr1, addr2] = await ethers.getSigners();
+    const owner = await ethers.getSigner(0);
+    const addr1 = await ethers.getSigner(1);
     console.log("address: ", owner.address);
+    console.log("address: ", addr1.address);
 
     console.log("Getting token");
     const token = await ethers.getContractFactory("CreepKidsNFT");
+    await token.connect(addr1);
     
     console.log("Deploying token");
     const hardhatToken = await token.deploy();
 
-    const ownerBalance = await hardhatToken.balanceOf(owner.address);
+    const ownerBalance = await hardhatToken.balanceOf(addr1.address);
     console.log("Owner balance: ", ownerBalance);
     
     expect(await hardhatToken.balanceOf(owner.address)).to.equal(ownerBalance);
