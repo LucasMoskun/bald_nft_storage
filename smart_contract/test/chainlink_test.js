6265d6ff495086c6473d6fa0193a5f49e814d73d
LucasMoskun <contact@lucasmoskun.com>
2021-08-20 10:13:31 +0100

    hellow world chainlink_test.js

---
+++ smart_contract/test/chainlink_test.js
@@ -0,0 +1,7 @@
+const {expect} = require("chai");
+
+describe("Chainlink Test", function () {
+  it("Running chainlink test", async function () {
+    console.log("Hello world from chainlink test");
+  });
+});
