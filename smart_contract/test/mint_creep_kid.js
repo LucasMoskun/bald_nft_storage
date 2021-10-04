bcf273d586b95e12df3e7d2b20f876ba333b2c2a
LucasMoskun <contact@lucasmoskun.com>
2021-10-04 23:09:32 +0100

    hardhat test to mint multiple nft's

---
+++ smart_contract/test/mint_creep_kid.js
@@ -0,0 +1,29 @@
+const {expect} = require("chai");
+
+describe("Creep Kid Mint Test", function () {
+  this.timeout(800000)
+  it("Minting creep kids", async function () {
+
+    console.log("Getting Signers");
+    const owner = await ethers.getSigner(0);
+
+    console.log("Getting token");
+    const token = await ethers.getContractFactory("CreepKidsNFT");
+
+    console.log("Connecting to contract");
+    const contractAddress = "0x865AAd4689e5aBA7D0610f3e17BAF4439EA059eC"
+    const contract = await token.attach(contractAddress);
+    await contract.connect(owner.address);
+
+    for(let i = 0; i < 78; i++){
+      console.log("Minting NFT count: ", i)
+      const mintTx = await contract.createCreepKid(owner.address);
+      const receipt = await mintTx.wait();
+      console.log("Token ID: ", receipt.events?.filter((x) => {return x.event == "TokenMintEvent"}));
+      console.log("Token URI: ", await contract.getMessage());
+    }
+
+    console.log("Finished minting");
+
+  });
+});
